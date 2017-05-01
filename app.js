f9279c97f698df2424a2af519c6450a191797583
Liviu Iancu <liviuiancu@gmx.com>
2017-05-01 17:41:14 +0300

    trying out events in vue

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ var app = new Vue({
   el: '#vue-app',
   data: {
     name: 'Strauss',
-    job: 'Dev',
-    website: 'http://google.com'
+    job: 'Composer',
+    age: 30,
+    website: 'http://google.com',
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function (time) {
       return 'good ' + time + ' ' + this.name;
+    },
+    add: function (inc) {this.age+=inc},
+    subtract: function (dec) {this.age-=dec},
+    updateXY: function (event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+    click: function () {
+      console.log('you clicked');
     }
   }
 });
