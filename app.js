f77652eee3f2089a809f46d63eb4c931caf8d170
Liviu Iancu <liviuiancu@gmx.com>
2017-05-01 10:15:00 +0300

    link to data

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-new Vue({
-  el: '#vue-app'
+var app = new Vue({
+  el: '#vue-app',
+  data: {
+    name: 'Strauss',
+    job: 'Dev',
+    website: 'http://google.com'
+  },
+  methods: {
+    greet: function (time) {
+      return 'good ' + time + ' ' + this.name;
+    }
+  }
 });
